dfe7605cba2ede32f11d45978d3f94ec723b8f55
Vignesh Kumar <qburst.vignesh@daimler.com>
2019-08-01 10:24:15 +0530

    Updates

---
+++ precache-manifest.19ed205f334f987ca434198f2c7a74c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1a1cf871b853f6094867ae130a47d41",
+    "url": "/PWA-Projects/index.html"
+  },
+  {
+    "revision": "0ac57d34a734ec78ed90",
+    "url": "/PWA-Projects/static/css/main.f76e5dd5.chunk.css"
+  },
+  {
+    "revision": "799f69f1277015682af2",
+    "url": "/PWA-Projects/static/js/2.9f91a5f4.chunk.js"
+  },
+  {
+    "revision": "0ac57d34a734ec78ed90",
+    "url": "/PWA-Projects/static/js/main.17354734.chunk.js"
+  },
+  {
+    "revision": "df197286c59016bb0fd4",
+    "url": "/PWA-Projects/static/js/runtime~main.ed807539.js"
+  }
+]);
